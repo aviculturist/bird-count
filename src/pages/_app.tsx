3944068da9e680c44d88e347f3f118c1054ac8a6
aviculturist <88842484+aviculturist@users.noreply.github.com>
2021-09-21 14:54:34 -0400

    removing legacy boilerplate. Bring back themes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react';
 import {Provider} from 'jotai';
+import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider as StyledComponentsTheme } from 'styled-components';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from '@styles/theme';
+import { lightTheme, darkTheme, scTheme } from '@styles/sc-theme';
 import type {AppProps} from 'next/app';
 import Head from 'next/head';
 
@@ -13,7 +18,12 @@ export default function BirdCountApp(props: AppProps) {
           <link href="/favicon.ico" rel="icon"/>
           <meta content="minimum-scale=1, initial-scale=1, width=device-width" name="viewport"/>
         </Head>
+        <ThemeProvider theme={theme}>
+          <StyledComponentsTheme theme={darkTheme}>
+            <CssBaseline />
         <Component {...pageProps} />
+        </StyledComponentsTheme>
+        </ThemeProvider>
       </React.Fragment>
     </Provider>
   );
