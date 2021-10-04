85514e3bc226c79b8205d1fdd7b6c2bc02947ed1
aviculturist <88842484+aviculturist@users.noreply.github.com>
2021-10-04 14:49:27 -0400

    reminder about import/export warning

--- src/store/user-locale.ts
+++ src/store/user-locale.ts
@@ -4,4 +4,5 @@ export const SUPPORTED_LOCALES = ['en', 'it', 'ru'] as const;
 export type Locale = typeof SUPPORTED_LOCALES[number];
 export const DEFAULT_LOCALE: Locale = 'en';
 export const userLocaleAtom = atom(<Locale | null>null);
+// TODO Should not import the named export 'messages' (reexported as 'DEFAULT_MESSAGES') from default-exporting module (only default export is available soon)
 export { messages as DEFAULT_MESSAGES } from '../../locale/en';
