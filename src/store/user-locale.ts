cb172615219363a6c3f88e9b64b5c42cbff9456a
aviculturist <88842484+aviculturist@users.noreply.github.com>
2021-10-06 07:39:47 -0400

    fix: missing file

--- src/store/user-locale.ts
+++ src/store/user-locale.ts
@@ -6,3 +6,9 @@ export const DEFAULT_LOCALE: Locale = 'en';
 export const userLocaleAtom = atom(<Locale | null>null);
 // TODO Should not import the named export 'messages' (reexported as 'DEFAULT_MESSAGES') from default-exporting module (only default export is available soon)
 export { messages as DEFAULT_MESSAGES } from '../../locale/en';
+export const CODE_TO_NAME: { [char: string]: string } = {
+  en: 'English',
+  ar: 'العربية',
+  it: 'Italiano',
+  ru: 'Русский',
+};
